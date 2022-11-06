454ca9bd3690e6e3654a3740e33188eb466fec12
kevz <kevz21@student.ubc.ca>
2022-11-06 14:12:32 -0800

    Added new code and installed react-router-dom

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,22 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Build from "./Build.js";
+import Weapon from "./Weapon.js";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Build
+        vigor = "Vigor: 40"
+        attunement = "Attunement: 20"
+        endurance = "Endurance: 40"
+        vitality = "Vitality: 20"
+        strength = "Strength: 20"
+        dexterity = "Dexterity: 40"
+        intelligence = "Intelligence: 40"
+        faith = "Faith: 20"
+        luck = "Luck: 12"
+        description = "Sample build for style A"
+        // weapon_array = {
+
+        weapon_link_1 = {"https://darksouls.fandom.com/wiki/Gotthard_Twinswords"}
+        weapon_image_1 = {"https://static.wikia.nocookie.net/darksouls/images/e/ea/Gotthard_Twinswords.png"}
+        weapon_name_1 = "Gotthard Twinswords"
+
+        weapon_link_2 = {"https://darksouls.fandom.com/wiki/Sellsword_Twinblades"}
+        weapon_image_2 = {"https://static.wikia.nocookie.net/darksouls/images/8/82/Sellsword_Twinblades.png"}
+        weapon_name_2 = "Sellsword Twinblades"
+
+        // }
+        />
+        
       </header>
     </div>
   );
--- my-website/src/Build.js
+++ my-website/src/Build.js
@@ -9,10 +9,35 @@ function Build(comps) {
 
     return (
         <div className="build">
-            <p className="description"></p>
+            <Header/>
+            <Button/>
+            <p className="vigor">{comps.vigor}</p>
+            <p className="attunement">{comps.attunement}</p>
+            <p className="endurance">{comps.endurance}</p>
+            <p className="vitality">{comps.vitality}</p>
+            <p className="strength">{comps.strength}</p>
+            <p className="dexterity">{comps.dexterity}</p>
+            <p className="intelligence">{comps.intelligence}</p>
+            <p className="faith">{comps.faith}</p>
+            <p className="luck">{comps.luck}</p>
+            <p className="description">{comps.description}</p>
+            <div className="weapon_array">
+                <Weapon
+                weapon_link = {comps.weapon_link_1}
+                weapon_image = {comps.weapon_image_1}
+                weapon_name = {comps.weapon_name_1}
+                />
+
+                <Weapon     
+                weapon_link = {comps.weapon_link_2}
+                weapon_image = {comps.weapon_image_2}
+                weapon_name = {comps.weapon_name_2}
+                />
+            </div>
         </div>
     )
 
 }
+
 
 export default Build
